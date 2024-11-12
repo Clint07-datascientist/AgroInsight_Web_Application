19023796369bd20ab5b2bbb92eae3e86089c9092
Clint07-datascientist <c.pikita@alustudent.com>
2024-11-12 13:03:34 +0200

    Encountering a case sensitive error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './styles/App.css';
 import Dashboard from './components/Dashboard';
 import SoilHealth from './components/SoilHealth';
@@ -6,17 +7,28 @@ import Weather from './components/Weather';
 import CropHealth from './components/CropHealth';
 import ResourceManagement from './components/ResourceManagement';
 import MarketInfo from './components/MarketInfo';
+import Login from './components/login';
+import AdminDashboard from './components/AdminDashboard';
+import Login from './components/Login';
 
 function App() {
+  const userRole = localStorage.getItem('userRole');
+
   return (
-    <div className="App">
-      <Dashboard />
-      <SoilHealth />
-      <Weather />
-      <CropHealth />
-      <ResourceManagement />
-      <MarketInfo />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard">
+            {userRole === 'farmer' ? <Dashboard /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/admin">
+            {userRole === 'admin' ? <AdminDashboard /> : <Redirect to="/login" />}
+          </Route>
+          <Redirect from="/" to="/login" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
